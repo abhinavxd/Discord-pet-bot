5e3eaaee542f86c08cce3a65ccb14617be7d1af4
Abhinav Raut <abhinavrautcs@gmail.com>
2021-10-25 23:11:59 +0530

    initial commit

---
+++ pet-bot.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const petPetGif = require('pet-pet-gif')
+const { Client, Intents, MessageAttachment } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async msg => {
+    const avatar = msg.author.avatar, id = msg.author.id;
+    const avatarUrl = `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`;
+    const animatedGif = await petPetGif(avatarUrl, { resolution: 200, })
+    if (msg.content === 'pet') {
+        const attachment = new MessageAttachment(animatedGif, 'profile-image.gif');
+        msg.reply({ files: [attachment] });
+    }
+});
+
+client.login(process.env.CLIENT_TOKEN); //login bot using token
